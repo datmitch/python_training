c459353ad5b6d6df75a23903a6da60e893fe3f23
dm072801 <Daniel.Mitchell@cerner.com>
2022-02-27 20:14:25 -0600

    Adding files from python training

---
+++ Ch5/inetdata_start.py
@@ -0,0 +1,13 @@
+#
+# Example file for retrieving data from the internet
+#
+import urllib.request
+
+def main():
+    webURL = urllib.request.urlopen("http://google.com")
+    print("result code: " + str(webURL.getcode()))
+    data = webURL.read()
+    print(data)
+
+if __name__ == "__main__":
+  main()
